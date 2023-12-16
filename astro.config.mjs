3ce63f5d96e40c97134b4f29bcbb7f0effe400d4
vish-rt <rawatvishal1707@gmail.com>
2023-12-17 02:01:46 +0530

    [FIX] uncomment base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://agnios.github.io',
-	// base: '/documentation',
+	base: '/documentation',
 	integrations: [
 		starlight({
 			title: 'AgniOS',
